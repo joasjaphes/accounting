c2a0a8ce5a558ba8757bedbd719e7148dc04a601
joas japhes <joasjaphes665@gmail.com>
2022-03-07 22:26:23 +0300

    frontend: Added angular material and bootstrap

--- apps/accounting-frontend/src/app/app.module.ts
+++ apps/accounting-frontend/src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent, LoginComponent],
   imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
