2c8747ee95c781c4b75f88a77bea8381bc9992f1
joas japhes <joasjaphes665@gmail.com>
2022-03-11 14:46:16 +0300

    modified login page

--- apps/accounting-frontend/src/app/app.module.ts
+++ apps/accounting-frontend/src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { MaterialModule } from './modules/material/material.module';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, LoginComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule],
   providers: [],
   bootstrap: [AppComponent],
 })
